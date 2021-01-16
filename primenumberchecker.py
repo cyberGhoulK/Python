f3ba2d9628493e82d4598ee44755389358b9c381
cyberGhoulK <71748495+cyberGhoulK@users.noreply.github.com>
2021-01-16 19:40:43 +0530

    Modifying primenumberchecker.py
    The new code promotes reusability by using functions so that it can be incorporated in other modules. The code is made consice and cleaner. Also, the bug relating to giving '2' is handled. This code uses, square theorem hence in range function 'int(math.sqrt(num))' is included which is an advantage if the number is large.

--- primenumberchecker.py
+++ primenumberchecker.py
@@ -1,11 +1,19 @@
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-else:
-  print(num,"is a prime number"
-else:
-  print(num,"is not a prime number")
+import math
+def isPrime(num, with_factors=False):
+    if num < 2:
+        # Returns False if number is 0, 1 or negative as these aren't prime
+        return [] if with_factors else False
+    if num in [2, 3]:
+        return [num] if with_factors else True
+    # Making a list of all factors
+    factors = [i for i in range(2, int(math.sqrt(num)) + 1) if num%i == 0]
+    # Checking and returning whether the factors list is empty or not.
+    return factors if with_factors else (not bool(factors))
+
+if __name__ == "__main__":
+    number = int(input("Enter number: "))
+    # Demonstrating UseCase1
+    print(number, "is", ("" if isPrime(number) else "not"), "a prime number")
+    # Demonstrating UseCase2
+    for fact in isPrime(number, with_factors=True):
+        print(f"{fact}*{number//fact}={number}")
